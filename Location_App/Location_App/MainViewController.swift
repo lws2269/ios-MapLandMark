384cbf33356710be68e8e20469f906eceac7f0d2
lws2269 <lws2269@kakao.com>
2023-03-06 11:40:54 +0900

    feat: navigation title 추가

--- Location_App/Location_App/MainViewController.swift
+++ Location_App/Location_App/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Location_App
 //
 //  Created by leewonseok on 2023/02/20.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "LandMark"
     }
 
 
